8eb483b9e26bb97fed2df74c64535018f28b623c
bekahdevore <devore.rc@gmail.com>
2016-05-25 09:45:23 -0400

    organized files

---
+++ JavaScript_Loops_Arrays_Objects/Section_1_Loops/Guess-randNumb1-10/app.js
@@ -0,0 +1,20 @@
+var randNumb = randomNumber(10); 
+var guess;
+var guessCount = 0; 
+var correctGuess = false; 
+
+function randomNumber(upper) {
+  return Math.floor( Math.random() * upper ) + 1;
+}
+
+do {
+	guess=prompt('I am thinking of a number between 1 and 10. What is it?')
+	guessCount += 1;
+	if(parseInt(guess) === randNumb) {
+		correctGuess = true; 
+
+	}
+
+} while (!correctGuess)
+document.write('<h1> You guessed the number!</h1>')
+document.write('It took you ' + guessCount + ' attempts, to guess the number ' + randNumb +'.')
