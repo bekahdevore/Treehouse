921b22de8c9019f81e25138400fb172d230ad5a2
bekahdevore <devore.rc@gmail.com>
2016-05-25 20:25:19 -0400

    todo list

--- project_stages/project_stage2_end/js/app.js
+++ project_stages/project_stage2_end/js/app.js
@@ -6,43 +6,91 @@ var addButton = document.getElementsByTagName("button")[0]; //first button
 var incompleteTasksHolder = document.getElementById("incomplete-tasks"); //incomplete-tasks
 var completedTasksHolder= document.getElementById("completed-tasks"); //completed-tasks
 
+var createNewTaskElement = function(taskString){
+    //Create List Item
+    var listItem = document.createElement("li");
+
+    //input (checkbox)
+    var checkBox = document.createElement("input");
+    //label
+    var label = document.createElement("label");
+
+    //input (text)
+    var editInput = document.createElement("input");
+
+    //button.edit
+    var editButton = document.createElement("button");
+    //button.delete
+    var deleteButton = document.createElement("button");
+    //Each element needs modifying
+    checkBox.type = "checkbox";
+    editInput.type ="text";
+
+    editButton.innerText = "Edit";
+    editButton.className = "edit";
+    deleteButton.innerText = "Delete";
+    deleteButton.className = "delete";
+    label.innerText = taskString;
+
+    //Appending each element
+    listItem.appendChild(checkBox);
+    listItem.appendChild(label);
+    listItem.appendChild(editInput);
+    listItem.appendChild(editButton);
+    listItem.appendChild(deleteButton);
+
+
+    
+    return listItem; 
+}
+
+
 //Add a new task
 var addTask = function() {
   console.log("Add task...");
   //When the button is pressed
   //Create a new list item with the text from #new-task:
-  //input (checkbox)
-    //label
-    //input (text)
-    //button.edit
-    //button.delete
-    //Each elements, needs modified and appended
+  var listItem = createNewTaskElement(taskInput.value);
+  
+  incompleteTasksHolder.appendChild(listItem);
+  bindTaskEvents(listItem, taskCompleted);
 }
 
 //Edit an existing task
 var editTask = function() {
   console.log("Edit task...");
-  //When the Edit button is pressed
-    //if the class of the parent is .editMode
-      //Switch from .editMode
-      //label text become the input's value
-    //else
-      //Switch to .editMode
-      //input value becomes the label's text
-    
-    //Toggle .editMode on the parent
+
+  var listItem = this.parentNode;
+  var editInput = listItem.querySelector("input[type=text]");
+  var label = listItem.querySelector("label");
+
+  var containsClass = listItem.classList.contains("editMode");
+  if(containsClass){
+      label.innerText = editInput.value;
+  } else {
+      editInput.value = label.innerText; 
+  }
+
+  listItem.classList.toggle("editMode");
 }
 
 //Delete an existing task
 var deleteTask = function() {
   console.log("Delete task...");
+  var listItem = this.parentNode;
+  var ul = listItem.parentNode;
   //When the Delete button is pressed
     //Remove the parent list item from the ul
+  ul.removeChild(listItem);
 }
 
 //Mark a task as complete
 var taskCompleted = function() {
   console.log("Task complete...");
+  var listItem = this.parentNode;
+  completedTasksHolder.appendChild(listItem);
+  bindTaskEvents(listItem, taskIncomplete);
+  
   //When the checkbox is checked
     //Append the task list item to the #completed-tasks
 }
@@ -50,6 +98,9 @@ var taskCompleted = function() {
 //Mark a task as incomplete
 var taskIncomplete = function() {
   console.log("Task incomplete...");
+  var listItem = this.parentNode;
+  incompleteTasksHolder.appendChild(listItem);
+  bindTaskEvents(listItem, taskCompleted);
   //When the checkbox is unchecked
     //Append the task list item to the #incomplete-tasks
 }
@@ -57,7 +108,14 @@ var taskIncomplete = function() {
 //Set the click handler to the addTask function
 addButton.onclick = addTask;
 
+var ajaxRequest = function(){
+  console.log("AJAX request");
+}
+addButton.onclick = ajaxRequest;
 
+for (var i = 0; i < incompleteTasksHolder.children.length; i++){
+  bindTaskEvents(incompleteTasksHolder.children[i], taskCompleted);
+}
 
 
 
