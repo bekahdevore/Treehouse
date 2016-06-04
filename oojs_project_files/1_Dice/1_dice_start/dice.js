d35dec64fe9a0a7a292275287e9002daefba2642
bekahdevore <devore.rc@gmail.com>
2016-06-04 16:35:39 -0400

    dice & calculator

--- oojs_project_files/1_Dice/1_dice_start/dice.js
+++ oojs_project_files/1_Dice/1_dice_start/dice.js
@@ -1,9 +1,19 @@
-function diceRoll() {
-  var sides = 6;
-  var randomNumber = Math.floor(Math.random() * sides) + 1;
-  console.log(randomNumber);
-}
+
 
 var dice = {
-
+	sides: 6,
+	roll: function(){
+	  var sides = 6;
+	  var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+	  return randomNumber;
+	}
 }
+
+// var dice10 = {
+// 	sides: 10,
+// 	roll: function(){
+// 	  var sides = 10;
+// 	  var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+// 	  console.log(randomNumber);
+// 	}
+// }
