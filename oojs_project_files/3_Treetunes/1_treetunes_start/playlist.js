7d2b68b2531d9883a8f81d018ea2d3602374b7e1
bekahdevore <devore.rc@gmail.com>
2016-06-06 21:49:53 -0400

    playlist and notes

--- oojs_project_files/3_Treetunes/1_treetunes_start/playlist.js
+++ oojs_project_files/3_Treetunes/1_treetunes_start/playlist.js
@@ -1,23 +1,35 @@
 function Playlist() {
-
+	this.songs = [];
+	this.nowPlayingIndex = 0;
 }
 
 Playlist.prototype.add = function() {
-
+	this.songs.push(song)
 };
 
 Playlist.prototype.play = function() {
-
+	var = currentSong = this.songs[this.nowPlayingIndex];
+	currentSong.play();
 };
 
 Playlist.prototype.stop = function(){
-
+	var = currentSong = this.songs[this.nowPlayingIndex];
+	currentSong.stop();
 };
 
 Playlist.prototype.next = function() {
+	this.stop();
+	this.nowPlayingIndex++;
+	if(this.nowPlayingIndex === this.songs.length){
+		this.play();
+	}
+	this.play();
 
 };
 
-Playlist.prototype.renderIn = function() {
-
-};
+playlist.prototype.renderInElement = function(list) {
+	list.innerHTML = "";
+	for(var i = 0; i < this.songs.length; i++) {
+		list.innerHTML += this.songs[i].toHTML();
+	}
+}
