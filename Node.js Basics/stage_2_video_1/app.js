e41366922e3565c64d00de1dc311147321417465
bekahdevore <devore.rc@gmail.com>
2016-06-14 21:03:00 -0400

    parson JSON start

--- Node.js Basics/stage_2_video_1/app.js
+++ Node.js Basics/stage_2_video_1/app.js
@@ -1,1 +1,38 @@
-console.log("Hello World");
+//Problem :Simple way to look at a users badge count and JS points
+//Solution: Use node to connect to Treehouse API to get profile information to print out
+
+var http = require("http"); 
+var username = "bekahcd"
+
+
+function printMessage(username, badgeCount, points) {     
+	var message = username + " has " + badgeCount + 
+			" total badge(s) and " + points +
+			" points in JavaScript";    
+	console.log(message); } 
+
+//Connect to API url (http://teamtreehouse.com/username.json) 
+var request = http.get("http://teamtreehouse.com/" + username + "json", function (response){
+		console.log(response.statusCode);
+	var body =  "";
+	//Read data
+	response.on('data', function (chunk) {
+		console.log(body += chunk);
+	});
+	response.on('end', function(){
+		console.log(body);
+		console.log(typeOf body);
+	})
+	//Parse data
+	//Print data
+});
+
+
+
+	
+
+	// request.on("error", function(error){
+	// console.error(error.message);
+	// });
+
+
