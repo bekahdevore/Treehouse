da0b5a1b696815771ddb45e0faa9fa73b97e67bb
bekahdevore <devore.rc@gmail.com>
2016-06-20 10:20:50 -0400

    home route complete

--- SimpleDynamicSiteWithNode/stage_2_video_1/app.js
+++ SimpleDynamicSiteWithNode/stage_2_video_1/app.js
@@ -5,18 +5,20 @@
 
 var http = require('http');
 http.createServer(function (request, response) {
- 
+  homeRoute(request, response);
 }).listen(3000);
 console.log('Server running at http://<workspace-url>/');
 
 //2. Handle HTTP route GET / and POST / i.e. Home
 function homeRoute(request, response) {
+  if(request.url === "/"){
   //if url == "/" && GET
     //show search
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.write("Header\n");
     response.write("Search\n");
     response.end("Footer\n");
+  }
   //if url == "/" && POST
     //redirect to /:username
 }
