abec432245f2fbdc1272ac6881b5bf85c68feef6
bekahdevore <devore.rc@gmail.com>
2016-07-12 19:53:39 -0400

    ng-blur & ng-class

--- Angular/1angularBasics/scripts/app.js
+++ Angular/1angularBasics/scripts/app.js
@@ -2,5 +2,15 @@ angular.module("todoListApp", [])
 	.controller('mainCtrl', function($scope){
 		$scope.helloWorld = function(){
 			console.log("Hello there! This is the helloworld contorller function!")
-		}
-	});
+	};
+
+	$scope.todos = [
+		{"name": "clean the house"},
+		{"name": "water the dog"},
+		{"name": "feed the lawn"}, 
+		{"name": "pay dem bills"}, 
+		{"name": "run"}, 
+		{"name": "swim"}
+	]
+
+});
